2c8ff2ec3087aa8c4a99a80fb89a1fed666a7a44
SKairinos <skairinos@gmail.com>
2023-12-20 16:51:58 +0000

    quick save

--- .github/scripts/contributing.py
+++ .github/scripts/contributing.py
@@ -5,6 +5,7 @@ Created on 19/12/2023 at 16:07:39(+00:00).
 Handles adding a new contributor to the contribution agreement.
 """
 
+import logging
 import re
 import subprocess
 from email.utils import parseaddr
@@ -13,88 +14,173 @@ from email.utils import parseaddr
 CONTRIBUTING_FILE_NAME = "CONTRIBUTING.md"
 CONTRIBUTORS_HEADER = "### 👨\u200d💻 Contributors 👩\u200d💻"
 
-# Get diff stats from main.
-result = subprocess.run(
-    [
-        "git",
-        "diff",
-        "main",
-        "--numstat",
-    ],
-    check=True,
-    stdout=subprocess.PIPE,
-)
 
-# Print diff stats.
-raw_stats = result.stdout.decode("utf-8")
-print(raw_stats)
-
-# Split diff stats.
-stats = raw_stats.splitlines()
-if len(stats) != 1:
-    raise AssertionError(f"Only {CONTRIBUTING_FILE_NAME} should be different.")
-
-# Parse git stats.
-stats = stats[0].split("\t")
-added_line_count, deleted_line_count, file_name = (
-    int(stats[0]),
-    int(stats[1]),
-    stats[2],
-)
-
-# Assert diff stats.
-if file_name != CONTRIBUTING_FILE_NAME:
-    raise AssertionError(f"Only {CONTRIBUTING_FILE_NAME} should be different.")
-if deleted_line_count > 0:
-    raise AssertionError(
-        "You cannot not modify or delete existing lines in"
-        f" {CONTRIBUTING_FILE_NAME}."
-    )
-if added_line_count == 100:  # != 1
-    raise AssertionError(f"You can only add one line in {CONTRIBUTING_FILE_NAME}.")
-
-
-def get_contributors():
-    """Extracts the contributors from the contributing agreement.
-
-    Returns:
-        A set of contributors.
+def assert_diff_stats():
+    """Assert that only the contribution agreement is different and only one
+    line was added to the contribution agreement.
     """
 
+    # Get raw diff stats from main.
+    diff_stats_str = subprocess.run(
+        [
+            "git",
+            "diff",
+            "main",
+            "--numstat",
+        ],
+        check=True,
+        stdout=subprocess.PIPE,
+    ).stdout.decode("utf-8")
+
+    logging.debug("Diff Stats:\n\n%s", diff_stats_str)
+
+    # Get diff stats per file.
+    diff_stats_per_file = diff_stats_str.splitlines()
+    # assert (
+    #     len(diff_stats_per_file) == 1
+    # ), f"Only {CONTRIBUTING_FILE_NAME} should be different."
+
+    # Parse and assert diff stats.
+    diff_stats = diff_stats_per_file[2].split("\t")  # TODO: 0
+    assert (
+        diff_stats[2] == CONTRIBUTING_FILE_NAME
+    ), f"Only {CONTRIBUTING_FILE_NAME} should be different."
+    assert int(diff_stats[1]) == 0, "You cannot modify or delete existing lines."
+    assert int(diff_stats[0]) == 1, "You must add just one line."
+
+
+def get_diff_line():
+    """Get the one differing line from the contribution agreement. Asserts that
+    the line is located in the list of contributors.
+
+    Returns:
+        The index and string of the one differing line from the contribution
+        agreement.
+    """
+
+    # Get raw diff from main.
+    diff_str = subprocess.run(
+        [
+            "git",
+            "--no-pager",
+            "diff",
+            "main",
+        ],
+        check=True,
+        stdout=subprocess.PIPE,
+    ).stdout.decode("utf-8")
+
+    diff_str = diff_str[
+        diff_str.index(
+            "diff --git a/CONTRIBUTING.md b/CONTRIBUTING.md\nindex 5b1b8c9..af7a8b8 100644"
+        ) : diff_str.index(
+            "diff --git a/codeforlife-deploy-appengine b/codeforlife-deploy-appengine\nindex b8b84e1..e6fccfc 160000"
+        )
+    ]
+
+    logging.debug("Diff:\n\n%s", diff_str)
+
+    # Match the diff line range.
+    diff_line_range = re.match(
+        r".*\@\@ -(\d+),(\d+) \+(\d+),(\d+) \@\@.*",
+        diff_str,
+        flags=re.DOTALL,
+    )
+    assert diff_line_range is not None, "Failed to match line-difference range."
+
+    # Assert diff line range is of size one.
+    original_line_start = int(diff_line_range.group(1))
+    new_line_start = int(diff_line_range.group(3))
+    assert original_line_start == new_line_start, "Line starts must be equal."
+
+    original_line_count = int(diff_line_range.group(2))
+    new_line_count = int(diff_line_range.group(4))
+    assert new_line_count - original_line_count == 1, "One line should be different."
+
+    diff_line_index = original_line_start + original_line_count
+
+    # Split contribution agreement into lines.
     with open(CONTRIBUTING_FILE_NAME, "r", encoding="utf-8") as contributing:
         lines = contributing.read().splitlines()
 
-    return set(lines[lines.index(CONTRIBUTORS_HEADER) + 2 :])
+    # Assert diff line is after the contributors header.
+    # NOTE: -1 to convert 1 based indexing to 0.
+    # NOTE: +1 because there should be a space after the header.
+    assert (
+        diff_line_index - 1 > lines.index(CONTRIBUTORS_HEADER) + 1
+    ), "Contributor must be added to the list of contributors."
+
+    diff_line = lines[diff_line_index - 1]
+
+    logging.debug("Diff Line:\n\n%s", diff_line)
+
+    return diff_line_index, diff_line
 
 
-# Get current contributors.
-contributors = get_contributors()
+def get_email_address(diff_line_index: int, diff_line: str):
+    """Get the email address from the diff line. Assert that the email address
+    used to sign the commit is the same as the signed email address.
 
-# Get previous contribution agreement.
-subprocess.run(
-    [
-        "git",
-        "checkout",
-        "main",
-        "--",
-        CONTRIBUTING_FILE_NAME,
-    ],
-    check=True,
-)
+    Args:
+        diff_line_index: The index of the differing line in the contribution
+        agreement. 1 based indexing.
+        diff_line: The differing line in the contribution agreement.
 
-# Get previous contributors.
-previous_contributors = get_contributors()
+    Returns:
+        The new contributor's email address.
+    """
 
-# Get new contributors.
-new_contributors = list(contributors.difference(previous_contributors))
+    # Get and assert the signed email address format.
+    _, signed_email_address = parseaddr(diff_line)
+    assert (
+        signed_email_address != ""
+        and re.match(r"[^@]+@[^@]+\.[^@]+", signed_email_address) is not None
+    ), "Invalid email address format."
 
-# Assert new contributors.
-if len(new_contributors) != 1:
-    raise AssertionError("There should be one new contributor.")
+    blame = subprocess.run(
+        [
+            "git",
+            "blame",
+            "-L",
+            f"{diff_line_index},{diff_line_index}",
+            CONTRIBUTING_FILE_NAME,
+            "--show-email",
+        ],
+        check=True,
+        stdout=subprocess.PIPE,
+    ).stdout.decode("utf-8")
 
-# Assert email address.
-_, email_address = parseaddr(new_contributors[0])
-if email_address == "" or re.match(r"[^@]+@[^@]+\.[^@]+", email_address) is None:
-    raise AssertionError("Invalid email address.")
+    logging.debug("Blame:\n\n%s", blame)
 
-# TODO: send verification email.
+    # Assert commit's author.
+    commit_author = re.match(rf".+ \(<(.+)> .+\) {re.escape(diff_line)}", blame)
+    assert commit_author is not None, "Failed to match commit author from git blame."
+
+    commit_author_email_address = commit_author.group(1)
+    assert (
+        signed_email_address.lower() == commit_author_email_address.lower()
+    ), "The signed email address must be equal to the commit author's."
+
+    return signed_email_address
+
+
+def send_verification_email(email_address: str):
+    """_summary_
+
+    Args:
+        email_address: _description_
+    """
+    pass
+
+
+def main():
+    """Runs the scripts."""
+
+    assert_diff_stats()
+    diff_line_index, diff_line = get_diff_line()
+    email_address = get_email_address(diff_line_index, diff_line)
+    send_verification_email(email_address)
+
+
+if __name__ == "__main__":
+    main()
