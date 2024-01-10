0803d06313e55cadaf29ed967c53763eb4394467
Stefan Kairinos <118008817+SKairinos@users.noreply.github.com>
2024-01-10 13:05:53 +0000

    Validate existing contributor (#57)
    * add contributing workflow
    
    * add contributing script
    
    * sds
    
    * add contributing script
    
    * quick save
    
    * fix workflow
    
    * fix trigger
    
    * remove example user
    
    * remove temp code
    
    * fix triggers
    
    * rename job
    
    * fetch main branch
    
    * keep .venv
    
    * setup python scripts dir
    
    * update pipeline
    
    * house keeping
    
    * test
    
    *  fix new line
    
    * fix fetch main
    
    * replace logging with print
    
    * test
    
    * fetch branch
    
    * set cwd
    
    * fix prints and paths
    
    * fix print
    
    * show response json
    
    * test
    
    * fix dd auth
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * remove test
    
    * test
    
    * test
    
    * test echo env var
    
    * test open
    
    * test multi env vars
    
    * keep venv
    
    * test send-email action
    
    * use action_path
    
    * cat file
    
    * cat file
    
    * fix action
    
    * set env vars
    
    * add comments
    
    * test json loads
    
    * test json loads pt2
    
    * test json loads pt3
    
    * send test email
    
    * house keeping
    
    * use send-email action
    
    * fix pipeline
    
    * rename file
    
    * fix module doc string
    
    * use html url
    
    * test
    
    * try review trigger
    
    * on pull_request_target
    
    * test
    
    * add env var
    
    * checkout
    
    * reviews
    
    * verify new contributor test
    
    * top level workflow
    
    * remove gh token secret
    
    * test
    
    * rename file
    
    * rename file
    
    * fix ref
    
    * rename file
    
    * test
    
    * test
    
    * fix ref
    
    * test
    
    * pipe output
    
    * print latest state
    
    * fix assertion
    
    * add .venv file
    
    * root dir per script
    
    * checkout: false
    
    * pass values
    
    * duplicate workflow
    
    * test
    
    * output email address
    
    * test
    
    * test
    
    * echo event name
    
    * test
    
    * not pull request
    
    * fix pr checkout
    
    * test
    
    * test
    
    * ref
    
    * force
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * set pr num
    
    * no secret
    
    * workflow call outputs
    
    * validate new contributor
    
    * test
    
    * test
    
    * test
    
    * rename
    
    * test
    
    * test
    
    * test
    
    * fix script
    
    * fix script
    
    * fix
    
    * test
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * tidy up
    
    * fix
    
    * create script
    
    * workflow
    
    * feeback
    
    * Merge branch 'new_contributor_validations' into validate_existing_contributor
    
    * Merge branch 'main' into validate_existing_contributor
    
    * rename files
    
    * add comment
    
    * fix
    
    * cd
    
    * path
    
    * echo
    
    * test
    
    * fix path
    
    * fix
    
    * echo pwd
    
    * wdir
    
    * test
    
    * rename files
    
    * test
    
    * names
    
    * test
    
    * test
    
    * test
    
    * fix paths
    
    * test
    
    * fix
    
    * test
    
    * test
    
    * test
    
    * test
    
    * remove input
    
    * test
    
    * aaa
    
    * bbb
    
    * input pull request
    
    * test
    
    * test
    
    * pleasework
    
    * pleasework
    
    * 123
    
    * should work
    
    * ref: main

---
+++ .github/scripts/python/validate-existing-contributors/__main__.py
@@ -0,0 +1,90 @@
+"""
+© Ocado Group
+Created on 08/01/2024 at 09:47:25(+00:00).
+
+Validate all contributors have signed the contribution agreement.
+"""
+
+import json
+import os
+import typing as t
+from email.utils import parseaddr
+
+PullRequest = t.Dict[str, t.Any]
+Contributors = t.Set[str]
+
+# pylint: disable-next=line-too-long
+CONTRIBUTING_FILE_NAME = "CONTRIBUTING.md"
+CONTRIBUTORS_HEADER = "### 👨\u200d💻 Contributors 👩\u200d💻"
+
+
+def get_inputs():
+    """Get script's inputs.
+
+    Returns:
+        A JSON object of the pull request.
+    """
+
+    pull_request: PullRequest = json.loads(os.environ["PULL_REQUEST"])
+
+    return pull_request
+
+
+def get_signed_contributors() -> Contributors:
+    """Get the contributors that have signed the contribution agreement.
+
+    Returns:
+        A set of the contributors' email addresses.
+    """
+
+    with open(
+        f"../../../../{CONTRIBUTING_FILE_NAME}",
+        "r",
+        encoding="utf-8",
+    ) as contributing:
+        lines = contributing.read().splitlines()
+
+    # NOTE: +2 because we don't want the header and its proceeding blank line.
+    lines = lines[lines.index(CONTRIBUTORS_HEADER) + 2 :]
+
+    return {parseaddr(line)[1] for line in lines}
+
+
+def assert_contributors(
+    pull_request: PullRequest,
+    signed_contributors: Contributors,
+):
+    """Assert that all contributors have signed the contribution agreement.
+
+    Args:
+        pull_request: The JSON object of the pull request.
+        signed_contributors: The contributors that have signed the contribution
+            agreement.
+    """
+
+    contributors: Contributors = {
+        author["email"]
+        for commit in pull_request["commits"]
+        for author in commit["authors"]
+    }
+
+    unsigned_contributors = contributors.difference(signed_contributors)
+
+    assert not unsigned_contributors, (
+        "The following contributors have not signed the agreement:"
+        f" {', '.join(unsigned_contributors)}."
+    )
+
+
+def main():
+    """Entry point."""
+
+    pull_request = get_inputs()
+
+    signed_contributors = get_signed_contributors()
+
+    assert_contributors(pull_request, signed_contributors)
+
+
+if __name__ == "__main__":
+    main()
