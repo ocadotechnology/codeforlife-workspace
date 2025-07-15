12c30230882df98cfe4c7c5e187b13b5465d6bb0
SKairinos <skairinos@gmail.com>
2025-07-15 11:35:06 +0000

    centralize everything into script

--- .github/scripts/issue/comment_created_or_edited.sh
+++ .github/scripts/issue/comment_created_or_edited.sh
@@ -1,93 +1,49 @@
-# A helper utility to set outputs on the GitHub step.
-function set_output() {
-  local output="$1=$2"
-  echo "Outputting to GitHub: \"$output\"."
-  echo "$output" >>$GITHUB_OUTPUT
+function eval_bool() {
+  if [ "$1" = "true" ]; then return 0; else return 1; fi
 }
 
-function extract_prompt() {
-  # Normalize the comment's body:
-  # 1. Remove any mention of "@cfl-bot".
-  # 2. Remove all leading or trailing spaces.
-  # 3. Substitute all 2+ recurring spaces with a single space.
-  # 4. Replace all upper-case characters with lower-case.
-  comment_body=$(
-    echo "$@" |
-      sed --regexp-extended '
-      s/(^|\W)@cfl-bot($|\W)/\1\2/g;
-      s/^[[:space:]]*|[[:space:]]*$//g;
-      s/[[:space:]]{2,}/ /g
-    ' |
-      tr '[:upper:]' '[:lower:]'
-  )
+function trim_spaces() {
+  echo "$@" | sed --regexp-extended 's/^[[:space:]]*|[[:space:]]*$//g'
+}
 
-  # Define the character set of each prompt.
-  # Used to remove all characters not in the set from the comment's body.
-  declare -A prompt_char_sets=(
-    ["$ASSIGN_ME_PROMPT_ID"]="[:alpha:][:blank:]"
-    ["$UNASSIGN_ME_PROMPT_ID"]="[:alpha:][:blank:]"
-    ["$READY_FOR_REVIEW_PROMPT_ID"]="[:alpha:][:blank:]"
-    ["$REQUIRES_CHANGES_PROMPT_ID"]="[:alpha:][:blank:]"
-    ["$LINK_PR_PROMPT_ID"]="[:alnum:][:blank:]"
-    ["$UNLINK_PR_PROMPT_ID"]="[:alnum:][:blank:]"
-  )
+function download_workspace_file() {
+  local branch="${branch:-"main"}"
+  local path="$1"
+  local save_to="$2"
 
-  # Define the POSIX regex pattern of each prompt.
-  # Used to check if the comment's body matches any of the prompts' pattern.
-  declare -A prompt_patterns=(
-    ["$ASSIGN_ME_PROMPT_ID"]="^assign\ me$"
-    ["$UNASSIGN_ME_PROMPT_ID"]="^unassign\ me$"
-    ["$READY_FOR_REVIEW_PROMPT_ID"]="^ready\ for\ review$"
-    ["$REQUIRES_CHANGES_PROMPT_ID"]="^requires\ changes$"
-    ["$LINK_PR_PROMPT_ID"]="^link\ pr\ ([0-9]+)$"
-    ["$UNLINK_PR_PROMPT_ID"]="^unlink\ pr\ ([0-9]+)$"
-  )
+  wget https://raw.githubusercontent.com/ocadotechnology/codeforlife-workspace/refs/heads/$branch/$path \
+    -O "$save_to"
+}
 
-  # Define a CSV of group names for each POSIX regex pattern that contains a group.
-  # Used to set outputs on the GitHub step.
-  declare -A prompt_patterns_group_name_csv=(
-    ["$LINK_PR_PROMPT_ID"]="pr-number"
-    ["$UNLINK_PR_PROMPT_ID"]="pr-number"
-  )
+function download_and_write_prompt_comment() {
+  local substitutions="$substitutions"
+  local prompt_id="$1"
+  local comment_md="$2"
 
-  # The name of the output that identifies a prompt.
-  prompt_id_key="id"
+  prompt_id="$(echo "$prompt_id" | sed 's/_/-/g')"
 
-  for prompt_id in "${!prompt_char_sets[@]}"; do
-    prompt_char_set="${prompt_char_sets[$prompt_id]}"
-    prompt_pattern="${prompt_patterns[$prompt_id]}"
+  local comment_path=".github/comments/issue/prompts/$prompt_id/$comment_md.md"
+  local save_to="$(echo "$comment_path" | sed 's/\//-/g')"
 
-    # Remove all characters not in the set from the comment's body.
-    comment_body_char_set=$(echo "$comment_body" | tr -dc "$prompt_char_set")
+  download_workspace_file "$comment_path" "$save_to"
 
-    # Check if the comment's body matches the prompt's pattern.
-    if [[ "$comment_body_char_set" =~ $prompt_pattern ]]; then
-      # Output the matching prompt.
-      set_output "$prompt_id_key" "$prompt_id"
+  # Write substitution to file.
+  echo "$substitutions" | while IFS= read -r line; do
+    IFS=',' read -ra pairs <<<"$line"
 
-      prompt_pattern_group_name_csv="${prompt_patterns_group_name_csv[$prompt_id]}"
+    for pair in "${pairs[@]}"; do
+      pair=$(trim_spaces "$pair")
+      if [ -z "$pair" ]; then continue; fi
 
-      # Check if a group-name CSV was provided for the pattern.
-      if [ -n "$prompt_pattern_group_name_csv" ]; then
-        # Convert the CSV to an array.
-        IFS=',' read -r -a prompt_pattern_group_names <<<"$prompt_pattern_group_name_csv"
+      local key="${pair%=*}"
+      local value="${pair#*=}"
 
-        for i in "${!prompt_pattern_group_names[@]}"; do
-          prompt_pattern_group_name="${prompt_pattern_group_names[$i]}"
-
-          # Output the group.
-          ((i++))
-          set_output "$prompt_pattern_group_name" "${BASH_REMATCH[$i]}"
-        done
-      fi
-
-      # Stop processing.
-      exit 0
-    fi
+      sed --in-place 's/{{ *'$key' *}}/'$value'/g' $save_to
+    done
   done
 
-  # Output that no matching prompt was found.
-  set_output "$prompt_id_key" ""
+  echo "Writing comment: \"$(cat $save_to)\"."
+  # gh issue comment $ISSUE_NUMBER --repo=$REPO --body-file=$save_to
 }
 
 function add_assignee() {
@@ -115,6 +71,8 @@ function remove_assignee() {
 }
 
 function add_label() {
+  local label="$@"
+
   gh label create "$label" \
     --repo=$REPO \
     --force \
@@ -127,10 +85,192 @@ function add_label() {
 }
 
 function remove_label() {
+  local label="$@"
+
   gh issue edit $ISSUE_NUMBER \
     --repo=$REPO \
     --remove-label="$label"
 }
 
-# Call function by name and pass the remaining arguments.
-if [ $# -gt 0 ]; then $1 ${@:2}; fi
+function has_label() {
+  local label="$@"
+
+  has_label=$(
+    gh issue view $ISSUE_NUMBER \
+      --repo=$REPO \
+      --json=labels \
+      --jq='.labels | map(.name) | contains(["'$label'"])'
+  )
+
+  return $(eval_bool "$has_label")
+}
+
+# Labels.
+ready_for_review_label="ready for review"
+
+# Prompt IDs.
+# Must be written in snake case and end with "_prompt_id".
+assign_me_prompt_id="assign_me"
+unassign_me_prompt_id="unassign_me"
+ready_for_review_prompt_id="ready_for_review"
+requires_changes_prompt_id="requires_changes"
+link_pr_prompt_id="link_pr"
+unlink_pr_prompt_id="unlink_pr"
+
+# Define the character set of each prompt.
+# Used to remove all characters not in the set from the comment's body.
+declare -A prompt_char_sets=(
+  ["$assign_me_prompt_id"]="[:alpha:][:blank:]"
+  ["$unassign_me_prompt_id"]="[:alpha:][:blank:]"
+  ["$ready_for_review_prompt_id"]="[:alpha:][:blank:]"
+  ["$requires_changes_prompt_id"]="[:alpha:][:blank:]"
+  ["$link_pr_prompt_id"]="[:alnum:][:blank:]"
+  ["$unlink_pr_prompt_id"]="[:alnum:][:blank:]"
+)
+
+# Define the POSIX regex pattern of each prompt.
+# Used to check if the comment's body matches any of the prompts' pattern.
+declare -A prompt_patterns=(
+  ["$assign_me_prompt_id"]="^assign\ me$"
+  ["$unassign_me_prompt_id"]="^unassign\ me$"
+  ["$ready_for_review_prompt_id"]="^ready\ for\ review$"
+  ["$requires_changes_prompt_id"]="^requires\ changes$"
+  ["$link_pr_prompt_id"]="^link\ pr\ ([0-9]+)$"
+  ["$unlink_pr_prompt_id"]="^unlink\ pr\ ([0-9]+)$"
+)
+
+# Auto-collect all prompt ids into an array.
+declare -a prompt_ids
+while read -r var_name; do
+  if [[ ! "$var_name" =~ _prompt_id$ ]]; then continue; fi
+
+  prompt_id="${!var_name}"
+
+  prompt_char_set="${prompt_char_sets[$prompt_id]}"
+  if [ -z "$prompt_char_set" ]; then
+    echo "A character set was not defined for prompt ID: \"$prompt_id\"."
+    exit 1
+  fi
+
+  prompt_pattern="${prompt_patterns[$prompt_id]}"
+  if [ -z "$prompt_pattern" ]; then
+    echo "A pattern was not defined for prompt ID: \"$prompt_id\"."
+    exit 1
+  fi
+
+  prompt_ids+=("$prompt_id")
+done < <(compgen -v)
+
+# Prompt handlers.
+# Must follow the naming convention "handle_{prompt_id}_prompt".
+function handle_assign_me_prompt() {
+  if ! eval_bool "$ISSUE_IS_OPEN"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$assign_me_prompt_id" \
+      "closed-state"
+  elif eval_bool "$ISSUE_HAS_COMMENTER_ASSIGNED"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$assign_me_prompt_id" \
+      "already-assigned"
+  elif eval_bool "$ISSUE_HAS_MAX_ASSIGNEES"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$assign_me_prompt_id" \
+      "max-assignees"
+  else
+    add_assignee
+  fi
+}
+function handle_unassign_me_prompt() {
+  if ! eval_bool "$ISSUE_HAS_COMMENTER_ASSIGNED"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$unassign_me_prompt_id" \
+      "not-assigned"
+  else
+    remove_assignee
+  fi
+}
+function handle_ready_for_review_prompt() {
+  if ! eval_bool "$ISSUE_IS_OPEN"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$ready_for_review_prompt_id" \
+      "closed-state"
+  elif ! eval_bool "$ISSUE_HAS_COMMENTER_ASSIGNED"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$ready_for_review_prompt_id" \
+      "not-assigned"
+  elif has_label "$ready_for_review_label"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$ready_for_review_prompt_id" \
+      "already-labelled"
+  else
+    color="#fbca04" \
+      description="This issue is awaiting review by a CFL team member." \
+      add_label "$ready_for_review_label"
+  fi
+}
+function handle_requires_changes_prompt() {
+  if ! eval_bool "$ISSUE_IS_OPEN"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$requires_changes_prompt_id" \
+      "closed-state"
+  elif ! eval_bool "$ISSUE_HAS_COMMENTER_ASSIGNED"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$requires_changes_prompt_id" \
+      "not-assigned"
+  elif ! has_label "$ready_for_review_label"; then
+    substitutions="contributor=@$USER_LOGIN" \
+      download_and_write_prompt_comment \
+      "$requires_changes_prompt_id" \
+      "not-labelled"
+  else
+    remove_label "$ready_for_review_label"
+  fi
+}
+function handle_link_pr_prompt() {
+  echo "TODO: implement"
+  exit 1
+}
+function handle_unlink_pr_prompt() {
+  echo "TODO: implement"
+  exit 1
+}
+
+# Normalize the comment's body:
+# 1. Remove any mention of "@cfl-bot".
+# 2. Remove all leading or trailing spaces.
+# 3. Substitute all 2+ recurring spaces with a single space.
+# 4. Replace all upper-case characters with lower-case.
+comment_body=$(
+  trim_spaces "$@" |
+    sed --regexp-extended '
+      s/(^|\W)@cfl-bot($|\W)/\1\2/g;
+      s/[[:space:]]{2,}/ /g
+    ' |
+    tr '[:upper:]' '[:lower:]'
+)
+
+for prompt_id in "${prompt_ids[@]}"; do
+  prompt_char_set="${prompt_char_sets[$prompt_id]}"
+  prompt_pattern="${prompt_patterns[$prompt_id]}"
+
+  # Remove all characters not in the set from the comment's body.
+  comment_body_char_set=$(echo "$comment_body" | tr -dc "$prompt_char_set")
+
+  # Check if the comment's body matches the prompt's pattern.
+  if [[ "$comment_body_char_set" =~ $prompt_pattern ]]; then
+    echo "Found matching prompt: \"$prompt_id\"."
+    "handle_${prompt_id}_prompt"
+    exit 0
+  fi
+done
+
+echo "No matching prompt was found."
