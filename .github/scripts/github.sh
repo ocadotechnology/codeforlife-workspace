9509ee5755d80d89a7a9883f59bfe3d62ab825bf
SKairinos <skairinos@gmail.com>
2025-07-22 14:28:47 +0000

    ignore rr

--- .github/scripts/github.sh
+++ .github/scripts/github.sh
@@ -57,6 +57,12 @@ function process_workspace_submodules() {
 function normalize_repo_name() {
   local repo_name="$1"
 
+  # TODO: fix repo name and delete this code block.
+  if [ "$repo_name" = "rapid-router" ]; then
+    echo "$repo_name"
+    return 0
+  fi
+
   if [[ ! "$repo_name" =~ ^$repo_name_prefix ]]; then
     repo_name="${repo_name_prefix}${repo_name}"
   fi
