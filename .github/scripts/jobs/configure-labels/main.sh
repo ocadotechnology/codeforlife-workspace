2f67b4f45b69849d5c3fd0ced57c24f381795efb
SKairinos <skairinos@gmail.com>
2025-07-28 10:28:34 +0000

    house keeping

--- .github/scripts/jobs/configure-labels/main.sh
+++ .github/scripts/jobs/configure-labels/main.sh
@@ -7,50 +7,14 @@ source .github/scripts/repositories.sh
 source .github/scripts/workspace.sh
 source .github/scripts/labels.sh
 
-labels_descriptor=".github/descriptors/labels.json"
 exit_code=0
 
-function check_labels_descriptor_schema() {
-  local labels_descriptor_schema="$(
-    realpath --relative-to="." "$(
-      dirname "$labels_descriptor"
-    )/$(
-      jq -r '.["$schema"]' "$labels_descriptor"
-    )"
-  )"
-
-  pip install check-jsonschema==0.33.* >/dev/null
-
-  check-jsonschema --schemafile="$labels_descriptor_schema" "$labels_descriptor"
-}
-
-function check_labels_descriptor_has_predefined_labels() {
-  local labels=(
-    "$cfl_bot_ignore_label"
-    "$ready_for_review_label"
-  )
-
-  for label in "${labels[@]}"; do
-    if ! eval_bool "$(
-      jq '.individual | has("'"$label"'")' "$labels_descriptor"
-    )"; then
-      exit=1 echo_error "Label: \"$label\" is missing in \"$labels_descriptor\"."
-    fi
-  done
-}
-
-function merge_individual_and_group_labels() {
-  jq '
-    .individual + (
-      .group |
-      [.[] | .colour as $colour | .labels | map_values(.colour = $colour)] |
-      add
-    )
-  ' "$labels_descriptor"
-}
-
 function configure_labels() {
-  echo_h2 "labels"
+  local repo_name="$1"
+
+  echo_h1 "$repo_name"
+
+  local repo="$(make_repo "$repo_name")"
 
   local index=1
 
@@ -81,28 +45,16 @@ function configure_labels() {
   done < <(echo "$labels" | jq -c 'to_entries | .[]')
 }
 
-function process_repo() {
-  local repo_name="$1"
-
-  echo_h1 "$repo_name"
-
-  local repo="$(make_repo "$repo_name")"
-
-  configure_labels
-}
-
 function main() {
-  check_labels_descriptor_schema
+  check_labels_descriptor
 
-  check_labels_descriptor_has_predefined_labels
-
-  labels="$(merge_individual_and_group_labels)"
+  labels="$(get_labels_from_descriptor)"
   labels_length="$(echo "$labels" | jq 'length')"
   echo_info "Discoverd $labels_length labels."
 
   if [ "$labels_length" -gt 0 ]; then
-    process_repo "$REPO_NAME"
-    process_workspace_submodules "process_repo"
+    configure_labels "$REPO_NAME"
+    process_workspace_submodules "configure_labels"
   fi
 
   exit $exit_code
--- .github/scripts/labels.sh
+++ .github/scripts/labels.sh
@@ -2,6 +2,8 @@
 
 source .github/scripts/general.sh
 
+labels_descriptor=".github/descriptors/labels.json"
+
 ready_for_review_label="ready for review"
 cfl_bot_ignore_label="bot ignore"
 
@@ -31,9 +33,52 @@ function make_label_filter_from_descriptor_group() {
   local group="$1"
 
   local label_csv="$(
-    jq -r '.group.'"$group"'.labels | keys | join(",")' \
-      .github/descriptors/labels.json
+    jq -r '.group.'"$group"'.labels | keys | join(",")' "$labels_descriptor"
   )"
 
   echo "$(make_label_filter "$label_csv")"
+}
+
+function check_labels_descriptor_schema() {
+  local labels_descriptor_schema="$(
+    realpath --relative-to="." "$(
+      dirname "$labels_descriptor"
+    )/$(
+      jq -r '.["$schema"]' "$labels_descriptor"
+    )"
+  )"
+
+  pip install check-jsonschema==0.33.* >/dev/null
+
+  check-jsonschema --schemafile="$labels_descriptor_schema" "$labels_descriptor"
+}
+
+function check_labels_descriptor_has_predefined_labels() {
+  local labels=(
+    "$cfl_bot_ignore_label"
+    "$ready_for_review_label"
+  )
+
+  for label in "${labels[@]}"; do
+    if ! eval_bool "$(
+      jq '.individual | has("'"$label"'")' "$labels_descriptor"
+    )"; then
+      exit=1 echo_error "Label: \"$label\" is missing in \"$labels_descriptor\"."
+    fi
+  done
+}
+
+function check_labels_descriptor() {
+  check_labels_descriptor_schema
+  check_labels_descriptor_has_predefined_labels
+}
+
+function get_labels_from_descriptor() {
+  jq '
+    .individual + (
+      .group |
+      [.[] | .colour as $colour | .labels | map_values(.colour = $colour)] |
+      add
+    )
+  ' "$labels_descriptor"
 }
