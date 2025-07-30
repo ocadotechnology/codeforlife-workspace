02c5fbd5605ebfc2ecc90afd0726777317165635
SKairinos <skairinos@gmail.com>
2025-07-30 12:02:11 +0000

    fix team names

--- .github/scripts/teams.sh
+++ .github/scripts/teams.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-full_team="cfl-core-team"
-devs_team="cfl-devs"
-senior_devs_team="cfl-senior-devs"
+full_team="cfl"
+dev_team="cfl-dev"
+senior_dev_team="cfl-senior-dev"
 ui_team="cfl-ui"
 ux_team="cfl-ux"
 edu_team="cfl-edu"
