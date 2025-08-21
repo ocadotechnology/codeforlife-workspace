85401fd85a5cfb9b11f4a26f438df3778d0abd98
Stefan Kairinos <skairinos@gmail.com>
2025-08-21 17:12:47 +0100

    Workspace 275 (#280)
    * quick save
    
    * quick save
    
    * fix
    
    * quick save
    
    * quick save
    
    * fix
    
    * save
    
    * save
    
    * emoji
    
    * more deps
    
    * add type definitions
    
    * remove alias
    
    * vite config
    
    * download workspace config file
    
    * prettier actions
    
    * check types
    
    * eslint check code
    
    * vite run tests
    
    * typo
    
    * code split
    
    * house keeping
    
    * standard snyk path
    
    * fix tsconfig paths
    
    * not required
    
    * fix download branch
    
    * not bash
    
    * install workspace dependencies
    
    * download yarn.lock
    
    * optionally install workspace package
    
    * download branch
    
    * fix naming
    
    * esModuleInterop
    
    * relative paths
    
    * allow explicit any
    
    * fix path
    
    * comments
    
    * update gitignore
    
    * standardize paths
    
    * ts expect error
    
    * ignore .workspace
    
    * fix
    
    * set to @main
    
    * feedback

---
+++ configs/frontend/eslint.config.js
@@ -0,0 +1,86 @@
+import js from "@eslint/js"
+import globals from "globals"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import ts from "typescript-eslint"
+
+// Base off of:
+// https://github.com/vitejs/vite/blob/main/packages/create-vite/template-react-ts/eslint.config.js
+export default ts.config(
+  {
+    // Global ignores.
+    // https://eslint.org/docs/latest/use/configure/configuration-files#globally-ignoring-files-with-ignores
+    ignores: [
+      "dist",
+      "**/*.d.ts",
+      "eslint.config.js",
+      "server.js",
+      // A special directory created during the runtime of our pipeline, into
+      // which any necessary files from the workspace are downloaded.
+      ".workspace",
+    ],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    extends: [
+      // Recommended config for JavaScript.
+      // https://www.npmjs.com/package/@eslint/js
+      js.configs.recommended,
+      // Recommended config for TypeScript.
+      // https://typescript-eslint.io/users/configs/#recommended-type-checked
+      ...ts.configs.recommendedTypeChecked,
+      // Recommended config for React.
+      // https://github.com/jsx-eslint/eslint-plugin-react?tab=readme-ov-file#flat-configs
+      react.configs.flat.recommended,
+      react.configs.flat["jsx-runtime"], // Add when using React 17+
+      // Enforces the rules of react hooks.
+      // https://www.npmjs.com/package/eslint-plugin-react-hooks
+      reactHooks.configs["recommended-latest"],
+      // Enforces that your components are structured in a way that support HMR.
+      // https://www.npmjs.com/package/eslint-plugin-react-refresh
+      reactRefresh.configs.vite,
+      // Turns off all rules that might conflict with Prettier.
+      // https://www.npmjs.com/package/eslint-config-prettier
+      eslintConfigPrettier,
+    ],
+    settings: { react: { version: "18.3" } },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parser: ts.parser,
+      parserOptions: {
+        // Expects each submodule to have theses tsconfigs present in the root.
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+      },
+    },
+    rules: {
+      "sort-imports": ["error", { allowSeparatedGroups: true }],
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { fixStyle: "inline-type-imports" },
+      ],
+      "@typescript-eslint/no-restricted-imports": [
+        "error",
+        {
+          paths: [
+            {
+              name: "react-redux",
+              importNames: ["useSelector", "useStore", "useDispatch"],
+              message:
+                "Please use pre-typed versions from `src/app/hooks.ts` instead.",
+            },
+          ],
+          patterns: [
+            {
+              group: ["codeforlife/src"],
+              message: "Please use `codeforlife` instead of `codeforlife/src`.",
+            },
+          ],
+        },
+      ],
+    },
+  },
+)
---
+++ configs/frontend/vite.config.ts
@@ -0,0 +1,41 @@
+/**
+ * Shared Vite configuration for all frontend services.
+ *
+ * Vite: https://vitest.dev/config/
+ * Vitest: https://vitest.dev/config/
+ */
+
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  plugins: [react()],
+  envDir: "env",
+  server: {
+    open: true,
+    host: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+    coverage: {
+      enabled: true,
+      provider: "istanbul",
+      reporter: ["html", "cobertura"],
+    },
+  },
+  optimizeDeps: {
+    // TODO: investigate which of these are needed
+    include: [
+      "@mui/x-date-pickers",
+      "@mui/x-date-pickers/AdapterDayjs",
+      "dayjs",
+      "dayjs/locale/en-gb",
+      "@mui/icons-material",
+      "yup",
+      "formik",
+    ],
+  },
+})
